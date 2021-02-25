126e7f560a005aa5c8c1071b4f7a78e129c83eb4
aritram1 <aritram1@gmail.com>
2021-02-25 06:57:31 +0530

    First Working copy

--- src/client/modules/my/weather/weather.js
+++ src/client/modules/my/weather/weather.js
@@ -1,34 +1,44 @@
 import { LightningElement, track, api } from 'lwc';
-import { helper, log, DEFAULT } from './weatherhelper.js';
+import { log, l, WeatherSchema, DefaultWeather, ResponseParser, Cities } from './weatherhelper.js';
 const WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?q=';
 const WEATHER_API = '&appid=dc14a4d0c2ab97c643acd3e8447fd074';
-
+const faker = require('faker');
 export default class Weather extends LightningElement{
-    //l = this.log;
-    //define properties
     @track currentWeather={};
     @track cached;
     @track error;
-    response = {};
-
-    //getters
+    @track lastupdated;
+    
     get timeSecondsAgo(){
         log(`timesecondsAgo called!`);
-        let timeinMS = Date.now() - this.currentWeather.lastUpdated || Date.now;
-        return timeinMS/1000;
+        log(this.lastUpdated);
+        let timeinMS = Date.now() - parseInt(this.lastUpdated) || Date.now;
+        return Math.timeinMS/1000;
     }
 
     //constructor
     constructor(){
         super();
-        this.currentWeather = Object.assign({}, DEFAULT);
+        this.currentWeather = Object.assign({}, WeatherSchema, DefaultWeather);
         this.cached = false;
+        this.lastupdated = Date.now();
     }
 
     //connectedCallback
     connectedCallback(){
-        setInterval(()=>{
-            let city = this.currentWeather.city;
+        setInterval(this.fetchWeather,
+            ()=>{
+            let city;
+            for(let i=0; i<5; i++){
+                let current = this.currentWeather.city;
+                let next = Cities[Math.floor((Math.random(1)*100)%3)];
+                if(next != current){
+                    city = next;
+                    if(i != 0) console.log(`After ${i} attempts resolved to : ${city}`);
+                    break;
+                }
+            }
+            
             let url = WEATHER_URL + city + WEATHER_API; //In the form [api.openweathermap.org/q=][cityName][&appid=<appId>]
             fetch(url)
             .then((result) => {
@@ -36,44 +46,23 @@ export default class Weather extends LightningElement{
                 return result.json();
             })
             .then(data=>{
-                log('inside fetch url success');
-                log(JSON.stringify(data));
-                this.response = data;
-                let a = Object.assign({}, DEFAULT, data);
-                console.log(a);
-                this.currentWeather = Object.assign({}, DEFAULT, data);
+                log(`Data received for ${city} as ${JSON.stringify(data)}`);
+                this.currentWeather = ResponseParser.parse(data);
+                //Object.assign(this.currentWeather, data);
+                //log(JSON.stringify(data));
+                //this.response = data;
+                //this.currentWeather = Object.assign({}, this.currentWeather, data);
             })
             .catch((error) => {
                 this.error = error;
                 log('inside error');
                 log(error);
             });
-        },5000);
+        },2000);
         
     }
 
-    // fetchWeather(){
-    //     console.log('hi');
-    //     let city = this.currentWeather.city;
-    //     let url = WEATHER_URL + city + WEATHER_API; //In the form [api.openweathermap.org/q=][cityName][&appid=<appId>]
-    //     fetch(url)
-    //     .then((result) => {
-    //         console.dir(result);
-    //         return result.json();
-    //     })
-    //     .then(data=>{
-    //         log('inside fetch url success');
-    //         log(data);
-    //         let a = Object.assign({}, DEFAULT, data);
-    //         console.log(a);
-    //         this.currentWeather = Object.assign({}, DEFAULT, data);
-    //     })
-    //     .catch((error) => {
-    //         this.error = error;
-    //         log('inside error');
-    //         log(error);
-    //     });
-    // }
-
+    //placeholder optimization
+    fetchWeather(){}
 
 }
--- src/client/modules/my/weather/weatherhelper.js
+++ src/client/modules/my/weather/weatherhelper.js
@@ -1,38 +1,45 @@
-export class helper{
-    message = 'default message';
-}
-
 export function log(msg){
     const styledMsg = '%c' + msg;
     const style = 'color: red';
     console.log(styledMsg, style);
 }
 
-export const DEFAULT = {
-    'id' : '',
-    'city' : 'Kolkata',
-    'location' : 'Kolkata',
-    'tempMinC' : 0,
-    'tempMaxC' : 0,//temp_max,
-    'humidity' : 0,//humidity,
-    'pressure' : 0;//pressure,
-    'lat' : lat,
-    'lon' : lon,
-    'windspeed' : windspeed,
-    'localtime' : timezone,
-    'sunrise' : sunrise,
-    'sunset' : sunset
-    }
+export function l(msg){
+    return log(msg);
+}
+
+export const Cities = ['Mumbai', 'Delhi', 'Paris', 'London', 'HongKong', 'Manila', 'Quebec'];
+
+export const DefaultWeather = {
     city : 'Kolkata',
-    location :'New town',
+    location :'Kolkata, India',
     tempMaxC : 25,
     tempMinC : 25,
     humidity : 60,
     url : 'https://google.com',
     lastUpdated : Date.now()
 };
+
+export const WeatherSchema = {
+    'id' : '',
+    'city' : '',
+    'location' : '',
+    'tempMinC' : 0,
+    'tempMaxC' : 0,
+    'humidity' : 0,
+    'pressure' : 0,
+    'lat' : 0.0,
+    'lon' : 0.0,
+    'windSpeed' : 0.00,
+    'localTime' : '',
+    'sunrise' : '',
+    'sunset' : '',
+    'lastUpdated' : Date.now(),
+    'url' : ''
+}
 export class ResponseParser{
-    static parse(resp){
+    Default
+    static parse(response){
         let {
             id, 
             name : city, 
@@ -41,26 +48,28 @@ export class ResponseParser{
             base,
             main : {temp, feels_like, temp_min, temp_max, pressure, humidity},
             visibility, 
-            wind : {windspeed, deg},
+            wind : {speed : windSpeed, deg},
             clouds : {all, dt},
             sys : {type, id_sys, country, sunrise, sunset},
             timezone, 
             cod 
-        } = resp;
+        } = response;
         return {
             'id' : id,
             'city' : city,
             'location' : `${city}, ${country}`,
-            'tempMinC' : temp_min,
-            'tempMaxC' : temp_max,
+            'tempMinC' : (temp_max-273).toLocaleString("en-US", { maximumFractionDigits: 2, minimumFractionDigits: 2 }),
+            'tempMaxC' : (temp_max-273).toFixed(2), //Second way of doing it
             'humidity' : humidity,
             'pressure' : pressure,
             'lat' : lat,
             'lon' : lon,
-            'windspeed' : windspeed,
-            'localtime' : timezone,
+            'windSpeed' : windSpeed,
+            'localTime' : timezone,
             'sunrise' : sunrise,
-            'sunset' : sunset
+            'sunset' : sunset,
+            'lastUpdated' : Date.now(),
+            'url' : ''
         }
     }
 }
