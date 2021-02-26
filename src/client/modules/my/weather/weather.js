4b6976eb3ee1a89642c8e9586e756a6f4821d65d
aritram1 <aritram1@gmail.com>
2021-02-26 23:36:22 +0530

    weather card first draft

--- src/client/modules/my/weather/weather.js
+++ src/client/modules/my/weather/weather.js
@@ -1,57 +1,20 @@
-import { LightningElement, track, api } from 'lwc';
-import { log, l, WeatherSchema, DefaultWeather, ResponseParser, Cities } from './weatherhelper.js';
+import { LightningElement, track } from 'lwc';
+import { ResponseParser } from './weatherhelper.js';
 const WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?q=';
 const WEATHER_API = '&appid=dc14a4d0c2ab97c643acd3e8447fd074';
 const DELAY = 2000; //in ms
-const faker = require('faker');
 export default class Weather extends LightningElement{
-    @track currentWeather;
-    @track cached; 
-    @track error; 
-    @track lastModified = Date.now;
-    @track attempts=0;
-    intervalId;
-    
-    //getters
-    get timeSecondsAgo(){
-        let diff = (Date.now() - this.lastModified)/1000;
-        return diff.toFixed(2);
-    }
+    @track currentWeather = {};
+    @track error;
+    @track city = 'Kolkata';
 
-    //constructor
     constructor(){
         super();
-        this.currentWeather = Object.assign({}, WeatherSchema, DefaultWeather);
-        this.cached = false;
-        this.lastModified = Date.now();
     }
 
-    //connectedCallback
     connectedCallback(){
-        this.intervalId = setInterval(()=>{
-            let city;
-            this.attempts = 0;
-            l('--------------------------------------------------');
-            for(let i=0; i<9; i++){
-                ('Attempt '+ i);
-                let current = this.currentWeather.city;
-                let next = Cities[Math.floor((Math.random(1)*100) % Cities.length)];
-                l('current >' + current);
-                l('new >' + next);
-                if(next != current){
-                    city = next;
-                    if(i != 0){
-                        console.log(`After ${i} attempts resolved to : ${city}`);
-                        this.attempts = i;
-                    }
-                    break;
-                }else{
-                    l('Same city found!');
-                }
-            }
-            l('----------------------------------------------------');
-            
-            let url = WEATHER_URL + city + WEATHER_API; //In the form [api.openweathermap.org/q=][cityName][&appid=<appId>]
+        setInterval(()=>{
+            let url = WEATHER_URL + this.city + WEATHER_API; //In the form [api.openweathermap.org/q=][cityName][&appid=<appId>]
             fetch(url)
             .then((result) => {
                 //console.dir(result);
@@ -60,23 +23,105 @@ export default class Weather extends LightningElement{
             .then(data=>{
                 //log(`Data received for ${city} as ${JSON.stringify(data)}`);
                 this.currentWeather = ResponseParser.parse(data); //Or Object.assign({}, this.currentWeather, data);
-                this.currentWeather.lastModified = Date.now();
+                this.error = undefined;
             })
             .catch((error) => {
-                log(`Encountered error during fetch : ${error}`);
+                console.log(`Encountered error during fetch : ${error}`);
                 this.error = error;
                 //throw error;
             });
-        },DELAY);
-
-        // let btnSv = document.getElementById('btnSv');
-        // btnSv.addEventListener('click', ()=>{
-        //     clearInterval(this.intervalId);
-        // });
-        
-    }
-
-    //placeholder optimization
-    //fetchWeather(){}
-
+        }, DELAY);
+    }   
 }
+
+
+
+
+
+
+
+
+
+
+// import { LightningElement, track } from 'lwc';
+// import { log, l, WeatherSchema, DefaultWeather, ResponseParser, Cities } from './weatherhelper.js';
+// const WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+// const WEATHER_API = '&appid=dc14a4d0c2ab97c643acd3e8447fd074';
+// const DELAY = 2000; //in ms
+// const faker = require('faker');
+// export default class Weather extends LightningElement{
+//     @track currentWeather;
+//     @track cached; 
+//     @track error; 
+//     @track lastModified = Date.now;
+//     @track attempts=0;
+//     intervalId;
+    
+//     //getters
+//     get timeSecondsAgo(){
+//         let diff = (Date.now() - this.lastModified)/1000;
+//         return diff.toFixed(2);
+//     }
+
+//     //constructor
+//     constructor(){
+//         super();
+//         this.currentWeather = Object.assign({}, WeatherSchema, DefaultWeather);
+//         this.cached = false;
+//         this.lastModified = Date.now();
+//     }
+
+//     //connectedCallback
+//     connectedCallback(){
+//         this.intervalId = setInterval(()=>{
+//             let city;
+//             this.attempts = 0;
+//             l('--------------------------------------------------');
+//             for(let i=0; i<9; i++){
+//                 ('Attempt '+ i);
+//                 let current = this.currentWeather.city;
+//                 let next = Cities[Math.floor((Math.random(1)*100) % Cities.length)];
+//                 l('current >' + current);
+//                 l('new >' + next);
+//                 if(next != current){
+//                     city = next;
+//                     if(i != 0){
+//                         console.log(`After ${i} attempts resolved to : ${city}`);
+//                         this.attempts = i;
+//                     }
+//                     break;
+//                 }else{
+//                     l('Same city found!');
+//                 }
+//             }
+//             l('----------------------------------------------------');
+            
+//             let url = WEATHER_URL + city + WEATHER_API; //In the form [api.openweathermap.org/q=][cityName][&appid=<appId>]
+//             fetch(url)
+//             .then((result) => {
+//                 //console.dir(result);
+//                 return result.json();
+//             })
+//             .then(data=>{
+//                 //log(`Data received for ${city} as ${JSON.stringify(data)}`);
+//                 this.currentWeather = ResponseParser.parse(data); //Or Object.assign({}, this.currentWeather, data);
+//                 this.currentWeather.lastModified = Date.now();
+//             })
+//             .catch((error) => {
+//                 log(`Encountered error during fetch : ${error}`);
+//                 this.error = error;
+//                 //throw error;
+//             });
+//         },DELAY);
+
+//         // let btnSv = document.getElementById('btnSv');
+//         // btnSv.addEventListener('click', ()=>{
+//         //     clearInterval(this.intervalId);
+//         // });
+        
+//     }
+
+//     //placeholder optimization
+//     //fetchWeather(){}
+
+// }
--- src/client/modules/my/weather/weatherhelper.js
+++ src/client/modules/my/weather/weatherhelper.js
@@ -1,44 +1,4 @@
-export function log(msg){
-    const styledMsg = '%c' + msg;
-    const style = 'color: red';
-    console.log(styledMsg, style);
-}
-
-export function l(msg){
-    return log(msg);
-}
-
-export const Cities = ['Mumbai', 'Delhi', 'Paris', 'London', 'HongKong', 'Manila', 'Quebec'];
-
-export const DefaultWeather = {
-    city : 'Kolkata',
-    location :'Kolkata, India',
-    tempMaxC : 25,
-    tempMinC : 25,
-    humidity : 60,
-    url : 'https://google.com',
-    lastUpdated : Date.now()
-};
-
-export const WeatherSchema = {
-    'id' : '',
-    'city' : '',
-    'location' : '',
-    'tempMinC' : 0,
-    'tempMaxC' : 0,
-    'feels_like' : 0,
-    'humidity' : 0,
-    'pressure' : 0,
-    'lat' : 0.0,
-    'lon' : 0.0,
-    'windSpeed' : 0.00,
-    'localTime' : '',
-    'sunrise' : '',
-    'sunset' : '',
-    'url' : ''
-}
-export class ResponseParser{
-    Default
+export class ResponseParser {
     static parse(response){
         let {
             id, 
@@ -54,22 +14,115 @@ export class ResponseParser{
             timezone, 
             cod 
         } = response;
+        let dayType = this.addDayType(humidity, feels_like, temp_max, temp_min);
+        let dayTypeImage =  './icon.gif';
         return {
             'id' : id,
-            'city' : city,
-            'location' : `${city}, ${country}`,
+            'city' : city, //used
+            'location' : `${city}, ${country}`, //used
             'tempMinC' : (temp_min-273).toLocaleString("en-US", { maximumFractionDigits: 2, minimumFractionDigits: 2 }),
-            'tempMaxC' : (temp_max-273).toFixed(2), //Second way of doing it
-            'feels_like' : (feels_like-273).toFixed(2),
+            'tempMaxC' : (temp_max-273).toFixed(2),
+            'feels_like' : (feels_like-273).toFixed(2),//used
             'humidity' : humidity,
             'pressure' : (pressure/1000).toFixed(2),
             'lat' : lat,
             'lon' : lon,
             'windSpeed' : windSpeed,
-            'localTime' : timezone,
+            'localTime' : timezone,//used
             'sunrise' : sunrise,
             'sunset' : sunset,
-            'url' : ''
+            'url' : '',
+            'dayType' : dayType //used
         }
     }
+    static addDayType(humidity, feels_like, temp_max, temp_min){
+        let dayTypes = ['Sunny', 'Overcast', 'Rainy'];
+        let r = Math.random(1)*100%3;
+        let dayType = dayTypes[r];
+        if(humidity > 60) dayType = 'Overcast';
+        if(feels_like-273 < 25) dayType = 'Cold';
+        if(feels_like-273 > 35) dayType = 'Warm';
+        return dayType;
+    }
 }
+
+
+
+
+
+
+// export function log(msg){
+//     const styledMsg = '%c' + msg;
+//     const style = 'color: red';
+//     console.log(styledMsg, style);
+// }
+
+// export function l(msg){
+//     return log(msg);
+// }
+
+// export const Cities = ['Mumbai', 'Delhi', 'Paris', 'London', 'HongKong', 'Manila', 'Quebec'];
+
+// export const DefaultWeather = {
+//     city : 'Kolkata',
+//     location :'Kolkata, India',
+//     tempMaxC : 25,
+//     tempMinC : 25,
+//     humidity : 60,
+//     url : 'https://google.com',
+//     lastUpdated : Date.now()
+// };
+
+// export const WeatherSchema = {
+//     'id' : '',
+//     'city' : '',
+//     'location' : '',
+//     'tempMinC' : 0,
+//     'tempMaxC' : 0,
+//     'feels_like' : 0,
+//     'humidity' : 0,
+//     'pressure' : 0,
+//     'lat' : 0.0,
+//     'lon' : 0.0,
+//     'windSpeed' : 0.00,
+//     'localTime' : '',
+//     'sunrise' : '',
+//     'sunset' : '',
+//     'url' : ''
+// }
+// export class ResponseParser{
+//     Default
+//     static parse(response){
+//         let {
+//             id, 
+//             name : city, 
+//             coord : {lon, lat}, 
+//             weather : {id_weather, main, description, icon},
+//             base,
+//             main : {temp, feels_like, temp_min, temp_max, pressure, humidity},
+//             visibility, 
+//             wind : {speed : windSpeed, deg},
+//             clouds : {all, dt},
+//             sys : {type, id_sys, country, sunrise, sunset},
+//             timezone, 
+//             cod 
+//         } = response;
+//         return {
+//             'id' : id,
+//             'city' : city,
+//             'location' : `${city}, ${country}`,
+//             'tempMinC' : (temp_min-273).toLocaleString("en-US", { maximumFractionDigits: 2, minimumFractionDigits: 2 }),
+//             'tempMaxC' : (temp_max-273).toFixed(2), //Second way of doing it
+//             'feels_like' : (feels_like-273).toFixed(2),
+//             'humidity' : humidity,
+//             'pressure' : (pressure/1000).toFixed(2),
+//             'lat' : lat,
+//             'lon' : lon,
+//             'windSpeed' : windSpeed,
+//             'localTime' : timezone,
+//             'sunrise' : sunrise,
+//             'sunset' : sunset,
+//             'url' : ''
+//         }
+//     }
+// }
