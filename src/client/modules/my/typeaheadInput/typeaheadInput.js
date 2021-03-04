2a7e55824d95325c687d291e38e36fef14b3729e
aritram1 <aritram1@gmail.com>
2021-03-05 01:41:11 +0530

    Interim commit

--- src/client/modules/my/typeaheadInput/typeaheadInput.js
+++ src/client/modules/my/typeaheadInput/typeaheadInput.js
@@ -1,6 +1,12 @@
 import { LightningElement, track, api } from 'lwc';
 import { log, l } from './helper.js';
+//const fs = require('fs');
+//const util = require('util');
+
+const mockPlaces = [{id: 1, name: 'Kolkata'}, {id: 2, name: 'Mumbai'}];
 export default class Typeaheadinput extends LightningElement{
+    @track input;
+    @track places = [];
     
     constructor(){
         super();
@@ -8,15 +14,43 @@ export default class Typeaheadinput extends LightningElement{
     }
 
     connectedCallback(){
-        let container = this.template.querySelector('div');
         console.log('inside cback');
-        console.log(container);
     }
 
 
     handleChange(event){
-        log('inside handlechange');
-        log(event.targetvalue);
+        let input = event.target.value;
+        if(input.length <= 3){
+            return;
+        }
+        else{
+            this.input = input;
+            
+            //const readFile = util.promisify(fs.readFile);
+            const url = 'http://localhost:3002/api/places?p=' + this.input;
+            fetch(url, {
+                method: 'GET',
+                mode: 'no-cors', // this is to prevent browser from sending 'OPTIONS' method request first
+                // redirect: 'follow',
+                // headers: new Headers({
+                //         'Content-Type': 'text/plain',
+                //         'X-My-Custom-Header': 'value-v',
+                //         'Authorization': 'Bearer ' + token,
+                // }),
+                // body: companyName
+            })
+            .then(data => {
+                console.log(this.process(data, this.input));
+            })
+            .catch(error=>{
+                console.log(`Error occurred : ${error}`);
+            });
+        }
+        
+    }
+
+    process(data, input){
+        return 'hi';
     }
         
 }
--- src/server/api.js
+++ src/server/api.js
@@ -2,6 +2,8 @@
 const compression = require('compression');
 const helmet = require('helmet');
 const express = require('express');
+const fetch = require('cross-fetch');
+const data = require('./places.json');
 
 const app = express();
 app.use(helmet());
@@ -10,8 +12,25 @@ app.use(compression());
 const HOST = process.env.API_HOST || 'localhost';
 const PORT = process.env.API_PORT || 3002;
 
-app.get('/api/v1/endpoint', (req, res) => {
-    res.json({ success: true });
+app.get('/api/places/', (req, res) => {
+    let p = req.params.p;
+    let places = data.places;
+    console.log('hey' + p);
+    console.log(places);
+    
+    let matchingPlaces = places.filter(place =>{
+        return place.indexOf(p) != -1;
+    });
+    res.json(matchingPlaces);
+    // fetch('./places.txt')
+    // .then(data =>{
+    //     console.log('Inside then block');
+    //     console.log(data);
+    //     res.json({ places: data });
+    // })
+    // .catch(error => { 
+    //     console.log(`Error occurred : ${error}`);
+    // });
 });
 
 app.listen(PORT, () =>
